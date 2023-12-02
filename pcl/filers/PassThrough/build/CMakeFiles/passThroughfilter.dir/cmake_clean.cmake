81341fa85625231dc47952b08bc1c165c3e1c398
BR.L <1819345998@qq.com>
2023-12-02 14:16:20 +0800

    init

---
+++ pcl/filers/PassThrough/build/CMakeFiles/passThroughfilter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/passThroughfilter.dir/main.cpp.o"
+  "CMakeFiles/passThroughfilter.dir/main.cpp.o.d"
+  "passThroughfilter"
+  "passThroughfilter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passThroughfilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pcl/filers/VoxelGrid/build/CMakeFiles/voxel_grid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxel_grid.dir/main.cpp.o"
+  "CMakeFiles/voxel_grid.dir/main.cpp.o.d"
+  "voxel_grid"
+  "voxel_grid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxel_grid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
